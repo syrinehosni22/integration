70c10a343c81296aac7946acd7de1c4f15cb742f
syrinehosni22 <hosnisyrine0@gmail.com>
2024-09-10 12:17:56 +0100

    back and forward working

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
+export default function Home (){
+    const navigate = useNavigate();
+
+    const goBack = ()=>{
+        // go Back
+        navigate(-1);
+      }
+      const next = () =>{
+        // next
+        navigate("/test");
+
+      }
+    return (
+        <div>
+             <div className='button-container'></div>
+      <button className='primary-button'onClick={goBack}>Back</button>
+      <button className='primary-button' onClick={next}>Forward</button>
+        </div>
+    )
+}
